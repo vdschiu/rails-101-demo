9c85f3641a7b146585bc6de4671358430f574f07
vds <vdschiu@gmail.com>
2016-02-29 14:10:32 +0800

    剛開始做第五章：可以在討論版裡發表文章

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,30 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "修改討論版成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "討論版已刪除"
+  end
+
   def create
     @group = Group.create(group_params)
 
