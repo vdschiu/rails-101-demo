87fda9a57e9b7a42c1896de00861ecb3357e47cf
vds <vdschiu@gmail.com>
2016-02-29 22:46:53 +0800

    第五章完成(終於)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'groups#index' #這行代表把 localhost:3000/groups 這個網址設成首頁
-  resources :groups
+  resources :groups do
+    resources :posts
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
